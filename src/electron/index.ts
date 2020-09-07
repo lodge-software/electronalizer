0420e94b1920b37c35b73efbe0b6f28c8d0151cf
kozr <zxzflow@gmail.com>
2020-09-07 16:11:12 -0700

    load website

--- src/electron/index.ts
+++ src/electron/index.ts
@@ -4,8 +4,8 @@ app.on('ready', () => {  // once electron has started up, create a window.
   const window = new BrowserWindow({ width: 800, height: 600 });
 
   // hide the default menu bar that comes with the browser window
-  window.setMenuBarVisibility(false);
+  window.setMenuBarVisibility(true);
 
   // load a website to display
-  window.loadURL(`https://google.com`);
+  window.loadURL(`file://${__dirname}/../website/index.html`);
 });
