d80d0d900c40665e11bf81c60701512a9546de21
kozr <zxzflow@gmail.com>
2020-09-07 15:32:25 -0700

    Initial commit

---
+++ src/electron/index.ts
@@ -0,0 +1,11 @@
+import { app, BrowserWindow } from 'electron';
+
+app.on('ready', () => {  // once electron has started up, create a window.
+  const window = new BrowserWindow({ width: 800, height: 600 });
+
+  // hide the default menu bar that comes with the browser window
+  window.setMenuBarVisibility(false);
+
+  // load a website to display
+  window.loadURL(`https://google.com`);
+});
