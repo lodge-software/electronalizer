ff520e2e24da68f0929acfacdb327d96a5341754
kozr <zxzflow@gmail.com>
2020-11-05 21:13:23 -0800

    uploaded bug

--- src/website/components/main/webrtc.tsx
+++ src/website/components/main/webrtc.tsx
@@ -1,68 +1,71 @@
 import React, { useEffect, useState } from 'react';
-import { Audio } from '../media';
+// import { Audio } from '../media';
 import { socket } from './sockets';
 
 const Main = function (): JSX.Element {
   // const [videoStream, setVideoStream] = useState(new MediaStream());
-  const [audioStream, setAudioStream] = useState<MediaStream>(new MediaStream());
-  const [startDisable, setStartDisable] = useState(false);
-  const [callDisable, setCallDisable] = useState(true);
+  const [localPeerConnection, setLocalPeerConnection] = useState(RTCPeerConnection.prototype);
+  // const [audioStream, setAudioStream] = useState<MediaStream>(new MediaStream());
+  // const [remoteAudioStream, setRemoteAudioStream] = useState<MediaStream>(new MediaStream());
+  const [callDisable, setCallDisable] = useState(false);
   const [hangupDisable, setHangupDisable] = useState(true);
-
-  let local: RTCPeerConnection;
 
   const offerOptions: RTCOfferOptions = {
     offerToReceiveAudio: true,
     offerToReceiveVideo: false,
   };
 
-  const start = async () => {
-    console.log('Requesting local stream');
-    setStartDisable(true);
-    try {
-      // const stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: true });
-      const audioStream = await navigator.mediaDevices.getUserMedia({ audio: true, video: false });
-      console.log('Received local stream');
-      setAudioStream(audioStream);
-      setCallDisable(false);
-    } catch (e) {
-      alert(`getUserMedia() error: ${e.name}`);
-    }
+  const createPeerConnection = () => {
+    console.log('Creating peer connection object');
+    const peerConnectionConfig = {
+      // iceServers: [{ urls: 'stun:stun.stunprotocol.org:3478' }, { urls: 'stun:stun.l.google.com:19302' }],
+    };
+    console.log('RTCPeerConnection configuration:', peerConnectionConfig);
+    setLocalPeerConnection(new RTCPeerConnection(peerConnectionConfig));
+    console.log('Created peer connection object');
   };
 
   const call = async () => {
     try {
-      setCallDisable(true);
-      setHangupDisable(false);
-      console.log('Start call');
-      const audioTracks = audioStream.getAudioTracks();
-      if (audioTracks.length > 0) {
-        console.log(`Using audio device: ${audioTracks[0].label}`);
-      }
+      // setCallDisable(true);
+      // setHangupDisable(false);
 
-      const configuration = {};
-      console.log('RTCPeerConnection configuration:', configuration);
-      local = new RTCPeerConnection(configuration);
+      console.log('Requesting local stream');
+      const localStream = await navigator.mediaDevices.getUserMedia(constraints);
 
-      audioTracks.forEach((track) => local.addTrack(track));
+      // setAudioStream(localStream);
 
-      const offer = await local.createOffer(offerOptions);
-      local.setLocalDescription(offer);
+      createPeerConnection();
 
-      socket.emit('offer', { socket: socket.id, desc: local.localDescription });
-
-      // Listens for answers from remote -> signaling server -> here
-      socket.on('answer', async (answer: any) => {
-        console.log(`Answer received from remote: ${answer.socket}`);
-        await local.setRemoteDescription(answer.desc).catch((e) => console.log(e));
+      console.log('Adding tracks');
+      localStream.getTracks().forEach((track) => {
+        localPeerConnection.addTrack(track, localStream);
       });
+      console.log('Added tracks');
+
+      console.log(`Creating offer`);
+      const offer = await localPeerConnection.createOffer(offerOptions);
+      localPeerConnection.setLocalDescription(offer);
+      console.log(`Created offer`);
+
+      console.log(`Emitting offer`);
+      socket.emit('offer', { data: offer });
+      console.log(`Emitted offer`);
+
+      // localPeerConnection.ontrack = (event: any) => {
+      //   setRemoteAudioStream(event.streams[0]);
+      // };
     } catch (e) {
       alert(`error starting call: ${e}`);
     }
   };
 
-  const close = async () => {
-    setStartDisable(false);
+  const constraints: MediaStreamConstraints = { audio: true, video: false };
+
+  // const retrieveMedia = async (): Promise<MediaStream> => {
+  // };
+
+  const hangup = async () => {
     setCallDisable(true);
     setHangupDisable(true);
   };
@@ -70,37 +73,53 @@ const Main = function (): JSX.Element {
   useEffect(() => {
     socket.on('connect', () => {
       console.log(`My current socket id is ${socket.id}`);
-      // Send offer to remote
     });
 
     socket.on('disconnect', () => {
       console.log(`The socket id: ${socket.id} has disconnected`);
     });
-    // Listens for any offers from remote -> signaling server -> here
-    socket.on('offer', async (currOffer: any) => {
-      console.log(`Request for connection received. SocketID is: ${currOffer.socket}`);
-      await local.setRemoteDescription(currOffer.desc);
-      local.setLocalDescription(await local.createAnswer());
-      socket.emit('answer', { socket: socket.id, desc: local.localDescription, dest: currOffer.socket });
-    });
-  });
 
-  // const getSelectedSdpSemantics = (): RTCConfiguration => {
-  //   return { sdpSemantics: "unified-plan" };
-  // };
+    socket.on('offer', async (currOffer: any) => {
+      console.log(`Request for connection received.`);
+
+      createPeerConnection();
+      console.log(localPeerConnection);
+      const localStream = await navigator.mediaDevices.getUserMedia(constraints);
+      // setAudioStream(localStream);
+
+      console.log('Adding tracks');
+      localStream.getTracks().forEach((track) => {
+        localPeerConnection.addTrack(track, localStream);
+      });
+      console.log('Added tracks');
+
+      await localPeerConnection
+        .setRemoteDescription(new RTCSessionDescription(currOffer.data))
+        .catch((e) => console.log(e));
+
+      const answer = await localPeerConnection.createAnswer();
+      await localPeerConnection.setLocalDescription(answer);
+      socket.emit('answer', {
+        data: answer,
+      });
+    });
+
+    socket.on('answer', async (answer: any) => {
+      console.log(`Answer received from remote: ${answer.data}`);
+      await localPeerConnection
+        .setRemoteDescription(new RTCSessionDescription(answer.data))
+        .catch((e) => console.log(e));
+    });
+  }, []);
 
   return (
     <div className="main">
-      {/* <Video srcObject={stream} id="localVideo"></Video> */}
-      <Audio srcObject={audioStream} id="localAudio"></Audio>
-      {/* <video id="remoteVideo" autoPlay></video> */}
-      <button disabled={startDisable} onClick={start}>
-        {'Start'}
-      </button>
+      {/* <Audio srcObject={audioStream} isAutoPlay={true} id="localAudio"></Audio> */}
+      {/* <Audio srcObject={remoteAudioStream} isAutoPlay={true} id="remoteAudio"></Audio> */}
       <button disabled={callDisable} onClick={call}>
         {'Call'}
       </button>
-      <button disabled={hangupDisable} onClick={close}>
+      <button disabled={hangupDisable} onClick={hangup}>
         {'Hang Up'}
       </button>
     </div>
--- src/website/components/media/Audio.tsx
+++ src/website/components/media/Audio.tsx
@@ -3,10 +3,11 @@ import React from 'react';
 
 type PropsType = AudioHTMLAttributes<HTMLAudioElement> & {
   srcObject: MediaStream;
+  isAutoPlay: boolean;
 };
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export default function Video({ srcObject, ...props }: PropsType) {
+export default function Video({ srcObject, isAutoPlay, ...props }: PropsType) {
   const refAudio = useRef<HTMLAudioElement>(null);
 
   useEffect(() => {
@@ -15,6 +16,9 @@ export default function Video({ srcObject, ...props }: PropsType) {
     }
   }, [srcObject]);
 
-  // return <audio ref={refAudio} {...props} autoPlay />;
-  return <audio ref={refAudio} {...props} />;
+  return isAutoPlay ? (
+    <audio ref={refAudio} {...props} autoPlay muted={true} />
+  ) : (
+      <audio ref={refAudio} muted={true} {...props} />
+    );
 }
